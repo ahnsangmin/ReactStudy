4e6373c389c7e19e995077b11087beab0284a3d1
Sangmin Ahn <ansang71@gmail.com>
2022-04-26 00:53:22 +0900

    버튼 달기

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
+import React, { useState } from 'react';
+import {SafeAreaView, Button} from 'react-native';
 import Box from './components/Box';
 const App = () => {
+
+    const [visible, setVisible] = useState(true);
+    const onPress=()=>{
+        setVisible(!visible);
+    }
+
     return (
         <SafeAreaView>
-          <Box rounded />
+          <Button title="토글" onPress={onPress} />
+          {!visible ? <Box rounded={true} size="large" color="blue"/>:null}
         </SafeAreaView>
     );
 
